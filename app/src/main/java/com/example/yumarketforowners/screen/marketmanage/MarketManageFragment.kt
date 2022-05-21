f6da078fb45e3bee59e994bf8bf117340a6c9143
ErroredPasta <ForeignServices3132@pm.me>
2022-05-21 19:00:23 +0900

    마켓 관리 화면의 fragment, view와 layout

---
+++ app/src/main/java/com/example/yumarketforowners/screen/marketmanage/MarketManageFragment.kt
@@ -0,0 +1,8 @@
+package com.example.yumarketforowners.screen.marketmanage
+
+import com.example.yumarketforowners.screen.ViewType
+import com.example.yumarketforowners.screen.base.BaseFragment
+
+class MarketManageFragment : BaseFragment<MarketManageFragmentView>() {
+    override val viewType: ViewType = ViewType.MARKET_MANAGE_FRAGMENT_VIEW
+}
---
+++ app/src/main/java/com/example/yumarketforowners/screen/marketmanage/MarketManageFragmentView.kt
@@ -0,0 +1,9 @@
+package com.example.yumarketforowners.screen.marketmanage
+
+import com.example.yumarketforowners.databinding.MarketManageFragmentBinding
+import com.example.yumarketforowners.screen.base.mvp.BaseView
+
+class MarketManageFragmentView(
+    binding: MarketManageFragmentBinding
+) : BaseView<MarketManageFragmentBinding>(binding) {
+}
