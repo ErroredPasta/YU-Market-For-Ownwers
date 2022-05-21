0d9264943d11d506b4f8f640b2304e5410bc0c19
ErroredPasta <ForeignServices3132@pm.me>
2022-05-21 19:00:35 +0900

    주문 목록 화면의 fragment, view와 layout

---
+++ app/src/main/java/com/example/yumarketforowners/screen/orderlist/OrderListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.yumarketforowners.screen.orderlist
+
+import com.example.yumarketforowners.screen.ViewType
+import com.example.yumarketforowners.screen.base.BaseFragment
+
+class OrderListFragment : BaseFragment<OrderListFragmentView>() {
+    override val viewType: ViewType = ViewType.ORDER_LIST_FRAGMENT_VIEW
+}
