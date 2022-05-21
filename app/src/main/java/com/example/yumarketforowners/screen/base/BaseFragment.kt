36b1119942abf277fa28664d93e65b477f3a9d1b
ErroredPasta <ForeignServices3132@pm.me>
2022-05-21 18:52:58 +0900

    Base activity와 fragment가 기본적으로 presenter 역할을 하도록 하고 자신이 가진 view를 type parameter로 명시하도록 함

---
+++ app/src/main/java/com/example/yumarketforowners/screen/base/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.example.yumarketforowners.screen.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.yumarketforowners.screen.ViewType
+import com.example.yumarketforowners.screen.base.mvp.BasePresenter
+import com.example.yumarketforowners.screen.base.mvp.BaseView
+import com.example.yumarketforowners.util.view.ViewFactory
+
+abstract class BaseFragment<V : BaseView<*>> : Fragment(), BasePresenter {
+    private var _view: V? = null
+    protected val view get() = _view!!
+
+    protected abstract val viewType: ViewType
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _view = ViewFactory.create(viewType, inflater, container)
+        return view.binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initState()
+    }
+
+    override fun onDestroyView() {
+        _view = null
+        beforeDestroy()
+        super.onDestroyView()
+    }
+
+    protected open fun initState() = Unit
+    protected open fun beforeDestroy() = Unit
+}
