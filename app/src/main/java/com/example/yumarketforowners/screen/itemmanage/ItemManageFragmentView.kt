8dd567e581054211d723fc0da966779f4494dfb4
ErroredPasta <ForeignServices3132@pm.me>
2022-05-21 19:00:11 +0900

    상품 관리 화면의 fragment, view와 layout

---
+++ app/src/main/java/com/example/yumarketforowners/screen/itemmanage/ItemManageFragmentView.kt
@@ -0,0 +1,10 @@
+package com.example.yumarketforowners.screen.itemmanage
+
+import com.example.yumarketforowners.databinding.ItemManageFragmentBinding
+import com.example.yumarketforowners.screen.base.mvp.BaseView
+
+class ItemManageFragmentView(
+    binding: ItemManageFragmentBinding
+) : BaseView<ItemManageFragmentBinding>(binding) {
+
+}
