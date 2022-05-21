9f4cf1dec2c31189b93ce92871357f4bd9f89148
ErroredPasta <ForeignServices3132@pm.me>
2022-05-21 19:02:12 +0900

    리뷰 관리 화면의 fragment, view와 layout

---
+++ app/src/main/java/com/example/yumarketforowners/screen/reviewmanage/ReviewManageFragment.kt
@@ -0,0 +1,8 @@
+package com.example.yumarketforowners.screen.reviewmanage
+
+import com.example.yumarketforowners.screen.ViewType
+import com.example.yumarketforowners.screen.base.BaseFragment
+
+class ReviewManageFragment : BaseFragment<ReviewManageFragmentView>() {
+    override val viewType: ViewType = ViewType.REVIEW_MANAGE_FRAGMENT_VIEW
+}
---
+++ app/src/main/java/com/example/yumarketforowners/screen/reviewmanage/ReviewManageFragmentView.kt
@@ -0,0 +1,9 @@
+package com.example.yumarketforowners.screen.reviewmanage
+
+import com.example.yumarketforowners.databinding.ReviewManageFragmentBinding
+import com.example.yumarketforowners.screen.base.mvp.BaseView
+
+class ReviewManageFragmentView(
+    binding: ReviewManageFragmentBinding
+) : BaseView<ReviewManageFragmentBinding>(binding) {
+}
