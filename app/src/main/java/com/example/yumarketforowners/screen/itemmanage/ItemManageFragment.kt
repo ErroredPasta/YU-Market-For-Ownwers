182d8ede8c93554a4a461f4f9d7dd243247a2af4
ErroredPasta <ForeignServices3132@pm.me>
2022-05-22 18:10:58 +0900

    상품을 관리하는 fragment에서 상품의 판매 상태에 따라 상품을 표시하는 fragment를 ViewPager로 출력하면 되므로 BaseFragmentView를 상속 받도록 함

--- app/src/main/java/com/example/yumarketforowners/screen/itemmanage/ItemManageFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.yumarketforowners.screen.itemmanage
-
-import com.example.yumarketforowners.screen.ViewType
-import com.example.yumarketforowners.screen.base.BaseFragment
-
-class ItemManageFragment : BaseFragment<ItemManageFragmentView>() {
-    override val viewType: ViewType = ViewType.ITEM_MANAGE_FRAGMENT_VIEW
-
-}
--- app/src/main/java/com/example/yumarketforowners/screen/itemmanage/ItemManageFragmentView.kt
+++ app/src/main/java/com/example/yumarketforowners/screen/itemmanage/ItemManageFragmentView.kt
@@ -1,10 +1,28 @@
 package com.example.yumarketforowners.screen.itemmanage
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import com.example.yumarketforowners.databinding.ItemManageFragmentBinding
-import com.example.yumarketforowners.screen.base.mvp.BaseView
+import com.example.yumarketforowners.screen.base.views.BaseFragmentView
 
-class ItemManageFragmentView(
-    binding: ItemManageFragmentBinding
-) : BaseView<ItemManageFragmentBinding>(binding) {
+class ItemManageFragmentView : BaseFragmentView<ItemManageFragmentBinding>() {
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = ItemManageFragmentBinding.inflate(inflater, container, false)
 
+//    private val adapter
+
+    override fun initState() {
+        super.initState()
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        val availability = arrayOf(true, false)
+
+        val fragments = availability.map {
+            ItemListFragmentPresenter.newInstance(it)
+        }
+    }
 }
