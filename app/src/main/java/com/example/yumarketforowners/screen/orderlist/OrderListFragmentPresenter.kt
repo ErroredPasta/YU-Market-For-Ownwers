d8232c71c26f77668884c41e8363ac440c7ec3d3
ErroredPasta <ForeignServices3132@pm.me>
2022-05-22 18:13:47 +0900

    Presenter를 나타내는 클래스들의 이름 끝에 presenter 추가

--- app/src/main/java/com/example/yumarketforowners/screen/orderlist/OrderListFragmentPresenter.kt
+++ app/src/main/java/com/example/yumarketforowners/screen/orderlist/OrderListFragmentPresenter.kt
@@ -1,8 +1,8 @@
 package com.example.yumarketforowners.screen.orderlist
 
 import com.example.yumarketforowners.screen.ViewType
-import com.example.yumarketforowners.screen.base.BaseFragment
+import com.example.yumarketforowners.screen.base.presenters.BaseFragmentPresenter
 
-class OrderListFragment : BaseFragment<OrderListFragmentView>() {
+class OrderListFragmentPresenter : BaseFragmentPresenter<OrderListFragmentView>() {
     override val viewType: ViewType = ViewType.ORDER_LIST_FRAGMENT_VIEW
 }
