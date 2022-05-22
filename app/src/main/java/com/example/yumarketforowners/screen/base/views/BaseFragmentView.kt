8da3140bceb22687a5f5f545cacd122d2134f31e
ErroredPasta <ForeignServices3132@pm.me>
2022-05-22 18:09:45 +0900

    Fragment에서 presenter를 필요로 하지 않고 단순히 정보를 출력할 경우에 사용할 base fragment

---
+++ app/src/main/java/com/example/yumarketforowners/screen/base/views/BaseFragmentView.kt
@@ -0,0 +1,36 @@
+package com.example.yumarketforowners.screen.base.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragmentView<VB : ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = getViewBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initState()
+    }
+
+    override fun onDestroyView() {
+        beforeDestroy()
+        super.onDestroyView()
+    }
+
+    protected open fun initState() = Unit
+    protected open fun beforeDestroy() = Unit
+    protected abstract fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+}
