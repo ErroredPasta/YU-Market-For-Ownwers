cf7fd1b57772002fd198756c3f0a2a06c8091725
ErroredPasta <ForeignServices3132@pm.me>
2022-06-02 16:24:52 +0900

    RecyclerView에 사용할 ModelRecyclerAdapter

---
+++ app/src/main/java/com/example/yumarketforowners/adapter/ModelRecyclerAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.yumarketforowners.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.yumarketforowners.adapter.listener.AdapterListener
+import com.example.yumarketforowners.adapter.viewholder.BaseViewHolder
+import com.example.yumarketforowners.adapter.viewholder.CellType
+import com.example.yumarketforowners.data.model.BaseModel
+import com.example.yumarketforowners.util.ViewHolderMapper
+
+class ModelRecyclerAdapter<M : BaseModel, VH : BaseViewHolder<*, M>>(
+    private var modelList: List<BaseModel>,
+    private val adapterListener: AdapterListener
+) : ListAdapter<BaseModel, VH>(BaseModel.ModelDiffCallback) {
+
+    override fun getItemViewType(position: Int): Int = modelList[position].cellType.ordinal
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH = ViewHolderMapper.map(
+        LayoutInflater.from(parent.context),
+        parent,
+        CellType.values()[viewType]
+    )
+
+    @Suppress("UNCHECKED_CAST")
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bindData(modelList[position] as M)
+        holder.bindListener(adapterListener)
+    }
+
+    override fun submitList(list: List<BaseModel>?) {
+        list?.let { modelList = it }
+        super.submitList(list)
+    }
+}
