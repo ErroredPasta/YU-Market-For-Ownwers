b71fc1a84d8b57e21473430fd21a4d79ab85cc65
ErroredPasta <ForeignServices3132@pm.me>
2022-06-02 16:06:42 +0900

    ViewPager를 사용하는 fragment의 base 클래스 정의

---
+++ app/src/main/java/com/example/yumarketforowners/screen/base/BaseViewPagerFragment.kt
@@ -0,0 +1,29 @@
+package com.example.yumarketforowners.screen.base
+
+import androidx.viewbinding.ViewBinding
+import androidx.viewpager2.widget.ViewPager2
+import com.example.yumarketforowners.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+abstract class BaseViewPagerFragment<VB : ViewBinding> : BaseFragment<VB>() {
+
+    protected abstract val innerFragments: List<BaseFragment<*>>
+    protected abstract val tabStrings: List<String>
+    protected lateinit var adapter: ViewPagerAdapter
+
+    protected fun initViewPagerAndTabLayout(viewPager: ViewPager2, tabLayout: TabLayout) {
+        if (!::adapter.isInitialized) {
+            adapter = ViewPagerAdapter(this, innerFragments)
+
+            viewPager.run {
+                this.adapter = this@BaseViewPagerFragment.adapter
+                offscreenPageLimit = innerFragments.size
+            }
+
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = tabStrings[position]
+            }
+        }
+    }
+}
