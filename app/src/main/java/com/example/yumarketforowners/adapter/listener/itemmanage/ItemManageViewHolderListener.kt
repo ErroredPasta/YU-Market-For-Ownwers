48ba254b0ba7592a802f6ebac08223ee53ecb89a
ErroredPasta <ForeignServices3132@pm.me>
2022-06-02 16:32:03 +0900

    상품 관리 화면에서 상품의 클릭 리스너의 기능을 정의할 interface

---
+++ app/src/main/java/com/example/yumarketforowners/adapter/listener/itemmanage/ItemManageViewHolderListener.kt
@@ -0,0 +1,7 @@
+package com.example.yumarketforowners.adapter.listener.itemmanage
+
+import com.example.yumarketforowners.adapter.listener.AdapterListener
+
+interface ItemManageViewHolderListener : AdapterListener {
+    // TODO: 2022.05.30 declare proper on click callbacks
+}
