2692554d48a34d1121e44247260c32a0f455575d
ErroredPasta <ForeignServices3132@pm.me>
2022-06-02 16:11:47 +0900

    상품 관리 화면에서 사용할 hilt module 정의

---
+++ app/src/main/java/com/example/yumarketforowners/di/fragment/ItemManageModule.kt
@@ -0,0 +1,30 @@
+package com.example.yumarketforowners.di.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.yumarketforowners.data.repository.itemmanage.ItemManageRepository
+import com.example.yumarketforowners.data.repository.itemmanage.ItemManageRepositoryImpl
+import com.example.yumarketforowners.screen.itemmanage.ItemManageContract
+import com.example.yumarketforowners.screen.itemmanage.ItemManagePresenter
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.scopes.FragmentScoped
+
+@Module
+@InstallIn(FragmentComponent::class)
+abstract class ItemManageModule {
+    @Binds
+    @FragmentScoped
+    abstract fun bindItemManagePresenter(presenter: ItemManagePresenter): ItemManageContract.Presenter
+
+    @Binds
+    @FragmentScoped
+    abstract fun bindItemManageRepository(repository: ItemManageRepositoryImpl): ItemManageRepository
+
+    companion object {
+        @Provides
+        fun provideItemManageView(fragment: Fragment) = fragment as ItemManageContract.View
+    }
+}
