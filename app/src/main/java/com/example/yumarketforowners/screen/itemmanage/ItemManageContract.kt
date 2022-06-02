4cd4e4de7b70dddd27c42a2174197b5f486a759b
ErroredPasta <ForeignServices3132@pm.me>
2022-06-02 16:03:30 +0900

    상품 관리에서 사용할 View와 Presenter가 어떤 기능을 가져야 하는지 interface와 상태를 나타낼 State 정의

---
+++ app/src/main/java/com/example/yumarketforowners/screen/itemmanage/ItemManageContract.kt
@@ -0,0 +1,26 @@
+package com.example.yumarketforowners.screen.itemmanage
+
+import androidx.annotation.StringRes
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
+
+interface ItemManageContract {
+    interface View {
+        fun loading(show: Boolean)
+        fun onRequestDataSuccess(items: List<ItemModel>)
+        fun onRequestDataError(@StringRes errorMessage: Int)
+    }
+
+    interface Presenter {
+        fun requestData()
+    }
+
+    sealed class State {
+        data class Success(
+            val items: List<ItemModel>
+        ) : State()
+
+        data class Error(
+            @StringRes val errorMessage: Int
+        ) : State()
+    }
+}
