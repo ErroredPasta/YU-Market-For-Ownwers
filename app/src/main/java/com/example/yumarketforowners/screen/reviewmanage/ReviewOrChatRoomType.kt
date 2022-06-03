6e617dcd5bfa1ce1e2b2c60bfe09616ff71862e1
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-03 23:37:34 +0900

    리뷰 관리 화면의 ViewPager에서 사용할 fragment의 종류를 정의할 enum 클래스

---
+++ app/src/main/java/com/example/yumarketforowners/screen/reviewmanage/ReviewOrChatRoomType.kt
@@ -0,0 +1,9 @@
+package com.example.yumarketforowners.screen.reviewmanage
+
+enum class ReviewOrChatRoomType(
+    val tabString: String
+) {
+    // TODO: 2022.05.30 replace hard coded strings to string resources
+    REVIEW("리뷰"),
+    CHAT_ROOM("문의 채팅"),
+}
