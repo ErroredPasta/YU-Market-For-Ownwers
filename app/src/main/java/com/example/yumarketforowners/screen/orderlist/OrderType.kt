d81a77af7b699ff02fc75a480ceaf019c04a9ab6
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 01:54:23 +0900

    주문의 상태를 나타낼 enum

---
+++ app/src/main/java/com/example/yumarketforowners/screen/orderlist/OrderType.kt
@@ -0,0 +1,10 @@
+package com.example.yumarketforowners.screen.orderlist
+
+enum class OrderType(
+    val tabString: String
+) {
+    // TODO: 2022.06.05 replace hardcoded string to string resources
+    PENDING("대기중"),
+    ACCEPTED("접수"),
+    DONE("완료"),
+}
