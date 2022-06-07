5d267813c77cc21637e0c713ba9b3aeadece4a1d
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-07 23:44:37 +0900

    상품 관리 화면에서 presenter를 inject받도록 수정

--- app/src/main/java/com/example/yumarketforowners/screen/itemmanage/ItemManageFragment.kt
+++ app/src/main/java/com/example/yumarketforowners/screen/itemmanage/ItemManageFragment.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.StringRes
+import com.example.yumarketforowners.adapter.ViewPagerAdapter
 import com.example.yumarketforowners.data.model.itemmanage.ItemModel
 import com.example.yumarketforowners.data.repository.itemmanage.ItemManageRepositoryImpl
 import com.example.yumarketforowners.databinding.ItemManageFragmentBinding
 import com.example.yumarketforowners.screen.base.BaseViewPagerFragment
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ItemManageFragment :
@@ -21,10 +23,9 @@ class ItemManageFragment :
         inflater, container, false
     )
 
+    @Inject
+    lateinit var presenter: ItemManageContract.Presenter
     private var dataLoaded = false
-    private val presenter: ItemManageContract.Presenter = ItemManagePresenter(
-        this, ItemManageRepositoryImpl()
-    )
 
     override val innerFragments = ItemAvailabilityType.values().map {
         ItemListFragment.newInstance(it.available)
@@ -60,21 +61,6 @@ class ItemManageFragment :
     override fun onRequestDataError(@StringRes errorMessage: Int) {
         // TODO: 2022.05.30 handle request data error
         Toast.makeText(context, getText(errorMessage), Toast.LENGTH_SHORT).show()
+        dataLoaded = false
     }
-
-//    private fun initViewPager() = with(binding) {
-//        // TODO: 2022.05.27 initialize viewpager
-//        if (::adapter.isInitialized.not()) {
-//            adapter = ViewPagerAdapter(this@ItemManageFragment, innerFragments)
-//
-//            itemManageViewPager.run {
-//                this.adapter = this@ItemManageFragment.adapter
-//                offscreenPageLimit = innerFragments.size
-//            }
-//
-//            TabLayoutMediator(itemManageTabLayout, itemManageViewPager) { tab, position ->
-//                tab.text = tabStrings[position]
-//            }.attach()
-//        }
-//    }
 }
