99c69c6b888f50e8cda9789f1c9661dc38ef38e9
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 01:54:31 +0900

    주문 목록 화면에서 ViewPager를 이용하여 보여줄 fragment

---
+++ app/src/main/java/com/example/yumarketforowners/screen/orderlist/OrderListViewPagerFragment.kt
@@ -0,0 +1,47 @@
+package com.example.yumarketforowners.screen.orderlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import com.example.yumarketforowners.adapter.ModelRecyclerAdapter
+import com.example.yumarketforowners.adapter.listener.orderlist.OrderViewHolderListener
+import com.example.yumarketforowners.data.model.orderlist.OrderModel
+import com.example.yumarketforowners.databinding.OrderListViewPagerFragmentBinding
+import com.example.yumarketforowners.screen.base.BaseFragment
+
+class OrderListViewPagerFragment : BaseFragment<OrderListViewPagerFragmentBinding>() {
+    companion object {
+        private const val ORDER_TYPE_KEY = "ORDER_TYPE_KEY"
+
+        fun newInstance(orderType: OrderType) = OrderListViewPagerFragment().apply {
+            arguments = bundleOf(ORDER_TYPE_KEY to orderType)
+        }
+    }
+
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = OrderListViewPagerFragmentBinding.inflate(inflater, container, false)
+
+    private val orderType by lazy {
+        requireArguments()[ORDER_TYPE_KEY] as OrderType
+    }
+
+    private val adapter by lazy {
+        ModelRecyclerAdapter<OrderModel>(
+            listOf(), object : OrderViewHolderListener {}
+        )
+    }
+
+    var orders: List<OrderModel> = listOf()
+        set(value) {
+            field = value.filter { it.orderType == orderType }
+            adapter.submitList(field)
+        }
+
+    override fun initState() {
+        binding.orderListRecyclerView.adapter = adapter
+
+        adapter.submitList(orders)
+    }
+}
