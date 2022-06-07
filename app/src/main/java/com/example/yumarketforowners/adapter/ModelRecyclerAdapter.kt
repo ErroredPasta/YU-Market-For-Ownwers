03f541c70df219d4d367f29fe097ed6827b66bcd
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-07 23:53:54 +0900

    ModelRecyclerAdapter에서 type parameter로 view holder를 받지 않도록 수정

--- app/src/main/java/com/example/yumarketforowners/adapter/ModelRecyclerAdapter.kt
+++ app/src/main/java/com/example/yumarketforowners/adapter/ModelRecyclerAdapter.kt
@@ -9,21 +9,21 @@ import com.example.yumarketforowners.adapter.viewholder.CellType
 import com.example.yumarketforowners.data.model.BaseModel
 import com.example.yumarketforowners.util.ViewHolderMapper
 
-class ModelRecyclerAdapter<M : BaseModel, VH : BaseViewHolder<*, M>>(
+class ModelRecyclerAdapter<M : BaseModel>(
     private var modelList: List<BaseModel>,
     private val adapterListener: AdapterListener
-) : ListAdapter<BaseModel, VH>(BaseModel.ModelDiffCallback) {
+) : ListAdapter<BaseModel, BaseViewHolder<*, M>>(BaseModel.ModelDiffCallback) {
 
     override fun getItemViewType(position: Int): Int = modelList[position].cellType.ordinal
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH = ViewHolderMapper.map(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*, M> = ViewHolderMapper.map(
         LayoutInflater.from(parent.context),
         parent,
         CellType.values()[viewType]
     )
 
     @Suppress("UNCHECKED_CAST")
-    override fun onBindViewHolder(holder: VH, position: Int) {
+    override fun onBindViewHolder(holder: BaseViewHolder<*, M>, position: Int) {
         holder.bindData(modelList[position] as M)
         holder.bindListener(adapterListener)
     }
