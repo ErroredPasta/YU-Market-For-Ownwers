55188eae644835d76a6f7dfa0fa34d063167104d
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 00:05:34 +0900

    리뷰 관리 repository에서 모든 데이터를 가져오는 method 이름 변경

--- app/src/main/java/com/example/yumarketforowners/data/repository/reviewmanage/ReviewOrChatRoomRepository.kt
+++ app/src/main/java/com/example/yumarketforowners/data/repository/reviewmanage/ReviewOrChatRoomRepository.kt
@@ -1,7 +1,8 @@
 package com.example.yumarketforowners.data.repository.reviewmanage
 
 import com.example.yumarketforowners.data.model.BaseModel
+import com.example.yumarketforowners.screen.reviewmanage.ReviewManageContract
 
 interface ReviewOrChatRoomRepository<M : BaseModel> {
-    fun getAll(marketId: Long): List<M>
+    fun getAllByMarketId(marketId: Long): ReviewManageContract.State
 }
