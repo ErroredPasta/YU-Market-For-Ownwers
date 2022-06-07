6dca840020be14ed67df10b968cd1d93c93c2921
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 01:53:30 +0900

    주문 목록 화면을 구현할 fragment

--- app/src/main/java/com/example/yumarketforowners/screen/orderlist/OrderListFragment.kt
+++ app/src/main/java/com/example/yumarketforowners/screen/orderlist/OrderListFragment.kt
@@ -2,10 +2,15 @@ package com.example.yumarketforowners.screen.orderlist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.example.yumarketforowners.data.model.orderlist.OrderModel
 import com.example.yumarketforowners.databinding.OrderListFragmentBinding
-import com.example.yumarketforowners.screen.base.BaseFragment
+import com.example.yumarketforowners.screen.base.BaseViewPagerFragment
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class OrderListFragment : BaseFragment<OrderListFragmentBinding>() {
+@AndroidEntryPoint
+class OrderListFragment : BaseViewPagerFragment<OrderListFragmentBinding>(),
+    OrderListContract.View {
 
     override fun getViewBinding(
         inflater: LayoutInflater,
@@ -13,4 +18,40 @@ class OrderListFragment : BaseFragment<OrderListFragmentBinding>() {
     ): OrderListFragmentBinding = OrderListFragmentBinding.inflate(
         inflater, container, false
     )
+
+    private var dataLoaded = false
+
+    override val innerFragments = OrderType.values().map(OrderListViewPagerFragment::newInstance)
+    override val tabStrings = OrderType.values().map { it.tabString }
+
+    @Inject
+    lateinit var presenter: OrderListContract.Presenter
+
+    override fun initState() {
+        initViewPagerAndTabLayout(
+            binding.orderListViewPager,
+            binding.orderListTabLayout
+        )
+
+        if (!dataLoaded) {
+            presenter.requestData(0)
+        }
+    }
+
+    override fun loading(show: Boolean) {
+        // TODO: 2022.06.05 handle loading
+    }
+
+    override fun onRequestDataSuccess(data: List<OrderModel>) {
+        for (innerFragment in innerFragments) {
+            innerFragment.orders = data
+        }
+
+        dataLoaded = true
+    }
+
+    override fun onRequestDataError(errorMessage: Int) {
+        // TODO: 2022.06.05 handle error
+        dataLoaded = false
+    }
 }
