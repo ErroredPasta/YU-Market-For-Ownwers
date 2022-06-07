266e9b7cdfca730bc06db5fde58af5593f4dbabf
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 00:00:13 +0900

    리뷰 관리 화면에서 필요한 동작들을 interface에 정의 및 수정

--- app/src/main/java/com/example/yumarketforowners/screen/reviewmanage/ReviewManageContract.kt
+++ app/src/main/java/com/example/yumarketforowners/screen/reviewmanage/ReviewManageContract.kt
@@ -2,19 +2,22 @@ package com.example.yumarketforowners.screen.reviewmanage
 
 import androidx.annotation.StringRes
 import com.example.yumarketforowners.data.model.BaseModel
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
 
 interface ReviewManageContract {
-    interface View {
-
+    interface View<M : BaseModel> {
+        fun loading(show: Boolean)
+        fun onRequestDataSuccess(data: List<M>)
+        fun onRequestDataError(@StringRes errorMessage: Int)
     }
 
     interface Presenter<M : BaseModel> {
-        fun requestData()
+        fun requestData(marketId: Long)
     }
 
     sealed class State {
         data class Success<M : BaseModel>(
-            val models: List<M>
+            val data: List<M>
         ) : State()
 
         data class Error(
