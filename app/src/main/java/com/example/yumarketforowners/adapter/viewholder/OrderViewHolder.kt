0ca31da42cf87b5fbbb0c14f01a3a8071d3abaa8
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 01:55:19 +0900

    주문을 나타낼 view holder

---
+++ app/src/main/java/com/example/yumarketforowners/adapter/viewholder/OrderViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.yumarketforowners.adapter.viewholder
+
+import com.example.yumarketforowners.adapter.listener.AdapterListener
+import com.example.yumarketforowners.data.model.orderlist.OrderModel
+import com.example.yumarketforowners.databinding.OrderViewHolderBinding
+
+class OrderViewHolder(
+    binding: OrderViewHolderBinding
+) : BaseViewHolder<OrderViewHolderBinding, OrderModel>(binding) {
+    override fun clear() {
+        // TODO: 2022.06.05 clear view holder
+    }
+
+    override fun bindData(model: OrderModel) {
+        super.bindData(model)
+        binding.orderModel = model
+    }
+
+    override fun bindListener(listener: AdapterListener) {
+        // TODO: 2022.06.05 bind click listener
+    }
+}
