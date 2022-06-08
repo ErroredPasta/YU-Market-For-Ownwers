8762a1d824bab6d6bb19677ba5b4753d15beab3e
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-09 02:05:46 +0900

    주문 view holder의 내부 RecyclerView adapter와 listener binding

--- app/src/main/java/com/example/yumarketforowners/adapter/viewholder/orderlist/OrderViewHolder.kt
+++ app/src/main/java/com/example/yumarketforowners/adapter/viewholder/orderlist/OrderViewHolder.kt
@@ -1,23 +1,28 @@
 package com.example.yumarketforowners.adapter.viewholder.orderlist
 
+import com.example.yumarketforowners.adapter.ModelRecyclerAdapter
 import com.example.yumarketforowners.adapter.listener.AdapterListener
+import com.example.yumarketforowners.adapter.listener.orderlist.OrderViewHolderListener
 import com.example.yumarketforowners.adapter.viewholder.BaseViewHolder
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
 import com.example.yumarketforowners.data.model.orderlist.OrderModel
 import com.example.yumarketforowners.databinding.ViewHolderOrderBinding
 
 class OrderViewHolder(
     binding: ViewHolderOrderBinding
 ) : BaseViewHolder<ViewHolderOrderBinding, OrderModel>(binding) {
-    override fun clear() {
-        // TODO: 2022.06.05 clear view holder
-    }
 
     override fun bindData(model: OrderModel) {
         super.bindData(model)
         binding.orderModel = model
+        binding.adapter = ModelRecyclerAdapter<ItemModel>(
+            model.orderItems
+        )
     }
 
     override fun bindListener(listener: AdapterListener) {
-        // TODO: 2022.06.05 bind click listener
+        if (listener is OrderViewHolderListener) {
+            binding.listener = listener
+        }
     }
 }
