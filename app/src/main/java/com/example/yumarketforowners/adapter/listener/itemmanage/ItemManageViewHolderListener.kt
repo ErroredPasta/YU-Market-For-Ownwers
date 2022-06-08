3da31bff1f69b32694d8076b60cde4fbe52c8257
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 23:23:44 +0900

    상품 view holder에서 사용할 listener의 call back method 정의

--- app/src/main/java/com/example/yumarketforowners/adapter/listener/itemmanage/ItemManageViewHolderListener.kt
+++ app/src/main/java/com/example/yumarketforowners/adapter/listener/itemmanage/ItemManageViewHolderListener.kt
@@ -1,7 +1,11 @@
 package com.example.yumarketforowners.adapter.listener.itemmanage
 
 import com.example.yumarketforowners.adapter.listener.AdapterListener
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
 
 interface ItemManageViewHolderListener : AdapterListener {
     // TODO: 2022.05.30 declare proper on click callbacks
+    fun onPlusButtonClick(item: ItemModel)
+    fun onMinusButtonClick(item: ItemModel)
+    fun onEditItemButtonClick(item: ItemModel)
 }
