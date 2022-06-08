b00292c49188b95048eed60df50676a5f851fe91
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-09 02:04:31 +0900

    주문 view holder에서 사용할 callback 정의

--- app/src/main/java/com/example/yumarketforowners/adapter/listener/orderlist/OrderViewHolderListener.kt
+++ app/src/main/java/com/example/yumarketforowners/adapter/listener/orderlist/OrderViewHolderListener.kt
@@ -1,6 +1,11 @@
 package com.example.yumarketforowners.adapter.listener.orderlist
 
 import com.example.yumarketforowners.adapter.listener.AdapterListener
+import com.example.yumarketforowners.data.model.orderlist.OrderModel
 
 interface OrderViewHolderListener : AdapterListener {
+    fun onTelePhoneNumberClicked(telePhoneNumber: String)
+    fun onAcceptOrderButtonClicked(order: OrderModel)
+    fun onRejectOrderButtonClicked(order: OrderModel)
+    fun onDeliveryDoneButtonClicked(order: OrderModel)
 }
