357cf1b1aaebd9d517a0b1b71dd5939dc00d91cf
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-09 02:07:05 +0900

    주문 상품 view holder 구현

---
+++ app/src/main/java/com/example/yumarketforowners/adapter/viewholder/orderlist/OrderItemViewHolder.kt
@@ -0,0 +1,19 @@
+package com.example.yumarketforowners.adapter.viewholder.orderlist
+
+import com.example.yumarketforowners.adapter.viewholder.BaseViewHolder
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
+import com.example.yumarketforowners.databinding.ViewHolderOrderItemBinding
+import com.example.yumarketforowners.extension.clear
+
+class OrderItemViewHolder(
+    binding: ViewHolderOrderItemBinding
+) : BaseViewHolder<ViewHolderOrderItemBinding, ItemModel>(binding) {
+    override fun clear() {
+        binding.orderItemImageView.clear()
+    }
+
+    override fun bindData(model: ItemModel) {
+        super.bindData(model)
+        binding.itemModel = model
+    }
+}
