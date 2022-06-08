6e1c12a77679baafe2e501c8718bc35257219564
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 23:26:04 +0900

    ViewPager를 사용하는 base fragment에서 ViewPagerAdapter 변수 이름 변경

--- app/src/main/java/com/example/yumarketforowners/screen/base/BaseViewPagerFragment.kt
+++ app/src/main/java/com/example/yumarketforowners/screen/base/BaseViewPagerFragment.kt
@@ -10,14 +10,14 @@ abstract class BaseViewPagerFragment<VB : ViewBinding> : BaseFragment<VB>() {
 
     protected abstract val innerFragments: List<BaseFragment<*>>
     protected abstract val tabStrings: List<String>
-    protected lateinit var adapter: ViewPagerAdapter
+    protected lateinit var viewPagerAdapter: ViewPagerAdapter
 
     protected fun initViewPagerAndTabLayout(viewPager: ViewPager2, tabLayout: TabLayout) {
-        if (!::adapter.isInitialized) {
-            adapter = ViewPagerAdapter(this, innerFragments)
+        if (!::viewPagerAdapter.isInitialized) {
+            viewPagerAdapter = ViewPagerAdapter(this, innerFragments)
 
             viewPager.run {
-                this.adapter = this@BaseViewPagerFragment.adapter
+                this.adapter = this@BaseViewPagerFragment.viewPagerAdapter
                 offscreenPageLimit = innerFragments.size
             }
 
