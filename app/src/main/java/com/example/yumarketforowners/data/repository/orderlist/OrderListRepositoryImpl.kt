f822920ade66f54ac2d241aa795822173237746b
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-09 02:02:42 +0900

    OrderModel의 property 변경 반영

--- app/src/main/java/com/example/yumarketforowners/data/repository/orderlist/OrderListRepositoryImpl.kt
+++ app/src/main/java/com/example/yumarketforowners/data/repository/orderlist/OrderListRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.example.yumarketforowners.data.repository.orderlist
 
+import com.example.yumarketforowners.adapter.viewholder.CellType
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
+import com.example.yumarketforowners.data.model.orderlist.DeliveryType
 import com.example.yumarketforowners.data.model.orderlist.OrderModel
 import com.example.yumarketforowners.screen.orderlist.OrderListContract.State
-import com.example.yumarketforowners.screen.orderlist.OrderType
+import com.example.yumarketforowners.screen.orderlist.OrderState
 import javax.inject.Inject
 
 
@@ -11,8 +14,24 @@ class OrderListRepositoryImpl @Inject constructor() : OrderListRepository {
         get() = (0..9).map {
             OrderModel(
                 id = it.toLong(),
+                orderItems = (0..1).map { itemIndex ->
+                    ItemModel(
+                        id = itemIndex.toLong(),
+                        name = "name $itemIndex",
+                        count = itemIndex,
+                        price = itemIndex,
+                        saleRatio = itemIndex,
+                        imageUrl = "https://picsum.photos/200",
+                        available = true,
+                        cellType = CellType.ORDER_ITEM_CELL
+                    )
+                },
                 orderId = "order $it",
-                orderType = OrderType.values()[it % 3]
+                deliveryType = DeliveryType.values()[it % 3],
+                orderTime = System.currentTimeMillis(),
+                telePhoneNumber = "telephone $it",
+                request = "request $it",
+                orderState = OrderState.values()[it % 3]
             )
         }
 
