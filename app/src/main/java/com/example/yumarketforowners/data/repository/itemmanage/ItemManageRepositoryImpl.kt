c1c6dea42a651232c4475c0b2d86a2f3876e428b
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-08 20:15:41 +0900

    ItemModel의 property 추가 반영

--- app/src/main/java/com/example/yumarketforowners/data/repository/itemmanage/ItemManageRepositoryImpl.kt
+++ app/src/main/java/com/example/yumarketforowners/data/repository/itemmanage/ItemManageRepositoryImpl.kt
@@ -5,15 +5,18 @@ import com.example.yumarketforowners.screen.itemmanage.ItemManageContract.State
 import javax.inject.Inject
 
 class ItemManageRepositoryImpl @Inject constructor() : ItemManageRepository {
-    private val testList get() = (0..9).map {
-        ItemModel(
-            id = it.toLong(),
-            name = "name $it",
-            price = it,
-            imageUrl = "https://picsum.photos/200",
-            available = it % 2 == 0
-        )
-    }
+    private val testList
+        get() = (0..9).map {
+            ItemModel(
+                id = it.toLong(),
+                name = "name $it",
+                price = it,
+                imageUrl = "https://picsum.photos/200",
+                saleRatio = it,
+                available = it % 2 == 0,
+                checkedForStateChange = false
+            )
+        }
 
     override fun getItemsByMarketId(marketId: Long): State = State.Success(items = testList)
 }
