05e8ab677d0ed590706572b81da6aa614f12c0cb
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-06-09 02:00:08 +0900

    OrderModel의 필요한 property 추가

--- app/src/main/java/com/example/yumarketforowners/data/model/orderlist/OrderModel.kt
+++ app/src/main/java/com/example/yumarketforowners/data/model/orderlist/OrderModel.kt
@@ -2,10 +2,16 @@ package com.example.yumarketforowners.data.model.orderlist
 
 import com.example.yumarketforowners.adapter.viewholder.CellType
 import com.example.yumarketforowners.data.model.BaseModel
-import com.example.yumarketforowners.screen.orderlist.OrderType
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
+import com.example.yumarketforowners.screen.orderlist.OrderState
 
 data class OrderModel(
     override val id: Long,
     val orderId: String,
-    var orderType: OrderType
+    val orderItems: List<ItemModel>,
+    val deliveryType: DeliveryType,
+    val orderTime: Long,
+    val telePhoneNumber: String,
+    val request: String,
+    var orderState: OrderState
 ) : BaseModel(id, CellType.ORDER_CELL)
