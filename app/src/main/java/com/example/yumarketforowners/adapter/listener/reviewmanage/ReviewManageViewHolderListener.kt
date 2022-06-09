4c412cf4f0925988da6bf355a2b6ff05253ed6ae
ErroredPasta <ForeignServices3132@pm.me>
2022-06-09 14:58:17 +0900

    리뷰 view holder에서 사용할 callback 함수 정의

--- app/src/main/java/com/example/yumarketforowners/adapter/listener/reviewmanage/ReviewManageViewHolderListener.kt
+++ app/src/main/java/com/example/yumarketforowners/adapter/listener/reviewmanage/ReviewManageViewHolderListener.kt
@@ -1,7 +1,8 @@
 package com.example.yumarketforowners.adapter.listener.reviewmanage
 
 import com.example.yumarketforowners.adapter.listener.AdapterListener
+import com.example.yumarketforowners.data.model.reviewmanage.ReviewModel
 
 interface ReviewManageViewHolderListener : AdapterListener{
-
+    fun onReplyClicked(review: ReviewModel)
 }
