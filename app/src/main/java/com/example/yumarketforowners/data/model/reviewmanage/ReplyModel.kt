4d86fba6ecd4d85ef7ce8047a2cf23f784d5d0e0
ErroredPasta <ForeignServices3132@pm.me>
2022-06-09 14:57:45 +0900

    리뷰의 답변을 나타낼 model

---
+++ app/src/main/java/com/example/yumarketforowners/data/model/reviewmanage/ReplyModel.kt
@@ -0,0 +1,8 @@
+package com.example.yumarketforowners.data.model.reviewmanage
+
+data class ReplyModel(
+    val replyWriter: String,
+    val profileImageUrl: String,
+    val content: String,
+    val replyTime: Long
+)
