70785750ea63d52a865eff9b003f96a99ba1a2aa
ErroredPasta <ForeignServices3132@pm.me>
2022-06-09 14:53:28 +0900

    Glide를 이용해 image의 rounding 처리를 할 수 있도록 변경

--- app/src/main/java/com/example/yumarketforowners/extension/ImageViewExtension.kt
+++ app/src/main/java/com/example/yumarketforowners/extension/ImageViewExtension.kt
@@ -1,13 +1,37 @@
 package com.example.yumarketforowners.extension
 
+import android.graphics.Bitmap
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.Transformation
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+
+// fade-in, fade-out의 애니메이션을 처리하기 위한 factory
+private val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
 
 fun ImageView.clear() = Glide.with(this).clear(this)
 
-fun ImageView.load(imageUrl: String) =
+fun ImageView.load(
+    url: String,
+    corner: Float = 0f,
+    scaleType: Transformation<Bitmap> = CenterInside()
+) =
     Glide.with(this)
-        .load(imageUrl)
+        .load(url)
+        .transition(DrawableTransitionOptions.withCrossFade(factory)) // fade-in 애니메이션
         .diskCacheStrategy(DiskCacheStrategy.ALL)
-        .into(this)
+        .run {
+            // 이미지의 라운딩 처리
+            if (corner > 0f) {
+                transform(
+                    scaleType, RoundedCorners(corner.fromDpToPx())
+                )
+            } else {
+                this
+            }
+        }.into(this)
