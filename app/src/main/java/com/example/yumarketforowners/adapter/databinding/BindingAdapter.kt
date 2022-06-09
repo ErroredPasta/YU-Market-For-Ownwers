857c67e2e2547eeb836d519f81db16616c0340ca
ErroredPasta <ForeignServices3132@pm.me>
2022-06-09 16:47:51 +0900

    채팅방 view holder에서 상대방 이름의 일부만 보여줄 때 사용할 기능 추가

--- app/src/main/java/com/example/yumarketforowners/adapter/databinding/BindingAdapter.kt
+++ app/src/main/java/com/example/yumarketforowners/adapter/databinding/BindingAdapter.kt
@@ -2,6 +2,7 @@ package com.example.yumarketforowners.adapter.databinding
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.yumarketforowners.extension.load
 
@@ -16,5 +17,15 @@ fun showIf(view: View, condition: Boolean) {
         View.VISIBLE
     } else {
         View.GONE
+    }
+}
+
+
+@BindingAdapter("opponentName")
+fun setOpponentName(textView: TextView, name: String) {
+    textView.text = if (name.length > 3) {
+        name.substring(0..3) + "***"
+    } else {
+        name[0] + "***"
     }
 }
