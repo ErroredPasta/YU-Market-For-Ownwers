47b78af1002c957d04515ea4436cffaee8bfc8be
ErroredPasta <ForeignServices3132@pm.me>
2022-06-09 15:01:46 +0900

    리뷰 repository의 test list에 property 추가 반영

--- app/src/main/java/com/example/yumarketforowners/data/repository/reviewmanage/ReviewRepositoryImpl.kt
+++ app/src/main/java/com/example/yumarketforowners/data/repository/reviewmanage/ReviewRepositoryImpl.kt
@@ -1,6 +1,11 @@
 package com.example.yumarketforowners.data.repository.reviewmanage
 
+import com.example.yumarketforowners.adapter.viewholder.CellType
+import com.example.yumarketforowners.data.model.itemmanage.ItemModel
+import com.example.yumarketforowners.data.model.orderlist.DeliveryType
+import com.example.yumarketforowners.data.model.orderlist.OrderModel
 import com.example.yumarketforowners.data.model.reviewmanage.ReviewModel
+import com.example.yumarketforowners.screen.orderlist.OrderState
 import com.example.yumarketforowners.screen.reviewmanage.ReviewManageContract.State
 import javax.inject.Inject
 
@@ -8,7 +13,31 @@ class ReviewRepositoryImpl @Inject constructor() : ReviewOrChatRoomRepository<Re
     private val testList get() = (0..9).map {
         ReviewModel(
             id = it.toLong(),
-            writer = "writer $it"
+            writer = "writer $it",
+            profileImageUrl = "https://picsum.photos/200",
+            content = "content $it",
+            order = OrderModel(
+                id = it.toLong(),
+                orderItems = (0..1).map { itemIndex ->
+                    ItemModel(
+                        id = itemIndex.toLong(),
+                        name = "name $itemIndex",
+                        count = itemIndex,
+                        price = itemIndex,
+                        saleRatio = itemIndex,
+                        imageUrl = "https://picsum.photos/200",
+                        available = true,
+                        cellType = CellType.ORDER_ITEM_CELL
+                    )
+                },
+                orderId = "order $it",
+                deliveryType = DeliveryType.values()[it % 3],
+                orderTime = System.currentTimeMillis(),
+                telePhoneNumber = "telephone $it",
+                request = "request $it",
+                orderState = OrderState.values()[it % 3]
+            ),
+            writeTime = System.currentTimeMillis()
         )
     }
 
