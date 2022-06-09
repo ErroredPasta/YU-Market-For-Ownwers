868bd927e1cd7e0aa4cb0e8fd16c1550a359808c
ErroredPasta <ForeignServices3132@pm.me>
2022-06-09 14:56:37 +0900

    data binding adapter의 기능 추가

--- app/src/main/java/com/example/yumarketforowners/adapter/databinding/BindingAdapter.kt
+++ app/src/main/java/com/example/yumarketforowners/adapter/databinding/BindingAdapter.kt
@@ -1,32 +1,20 @@
 package com.example.yumarketforowners.adapter.databinding
 
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.example.yumarketforowners.extension.load
-import com.example.yumarketforowners.screen.orderlist.OrderState
 
-@BindingAdapter("app:imageUrl")
-fun setImageByUrl(imageView: ImageView, imageUrl: String) = imageView.load(imageUrl)
+@BindingAdapter(value = ["imageUrl", "cornerRadius"], requireAll = false)
+fun setImageByUrl(imageView: ImageView, imageUrl: String, corner: Float) =
+    imageView.load(imageUrl, corner)
 
 
-@BindingAdapter("app:showIfPending")
-fun showIfPending(button: Button, orderState: OrderState) {
-    button.visibility =
-        if (orderState == OrderState.PENDING) {
-            View.VISIBLE
-        } else {
-            View.GONE
-        }
-}
-
-@BindingAdapter("app:showIfAccepted")
-fun showIfAccepted(button: Button, orderState: OrderState) {
-    button.visibility =
-        if (orderState == OrderState.ACCEPTED) {
-            View.VISIBLE
-        } else {
-            View.GONE
-        }
+@BindingAdapter("showIf")
+fun showIf(view: View, condition: Boolean) {
+    view.visibility = if (condition) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
 }
