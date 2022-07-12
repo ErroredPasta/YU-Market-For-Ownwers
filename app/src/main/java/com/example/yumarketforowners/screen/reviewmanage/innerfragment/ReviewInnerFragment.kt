18094466edab467cfe9103934682912499f189bf
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-07-12 22:36:43 +0900

    리뷰 관리 화면에서 리뷰를 띄워줄 fragment

---
+++ app/src/main/java/com/example/yumarketforowners/screen/reviewmanage/innerfragment/ReviewInnerFragment.kt
@@ -0,0 +1,31 @@
+package com.example.yumarketforowners.screen.reviewmanage.innerfragment
+
+import android.widget.Toast
+import com.example.yumarketforowners.adapter.ModelRecyclerAdapter
+import com.example.yumarketforowners.adapter.listener.reviewmanage.ReviewViewHolderListener
+import com.example.yumarketforowners.data.model.reviewmanage.ReviewModel
+import com.example.yumarketforowners.screen.reviewmanage.ReviewManageContract
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ReviewInnerFragment : BaseReviewInnerFragment<ReviewModel>() {
+    @Inject
+    lateinit var presenter: ReviewManageContract.Presenter<ReviewModel>
+
+    override val adapter: ModelRecyclerAdapter<ReviewModel> by lazy {
+        ModelRecyclerAdapter(
+            emptyList(),
+            object : ReviewViewHolderListener {
+                override fun onReplyClicked(review: ReviewModel) {
+                    Toast.makeText(context, "$review reply clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    override fun requestData() {
+        // TODO: 2022.07.10 request by market id
+        presenter.requestData(0)
+    }
+}
