937c5a1fde779a10bb642f9be5eb8faf3a8db410
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-07-12 22:36:24 +0900

    리뷰 관리 화면에서 문의 채팅을 출력할 fragment

---
+++ app/src/main/java/com/example/yumarketforowners/screen/reviewmanage/innerfragment/ChatRoomInnerFragment.kt
@@ -0,0 +1,37 @@
+package com.example.yumarketforowners.screen.reviewmanage.innerfragment
+
+import android.widget.Toast
+import com.example.yumarketforowners.adapter.ModelRecyclerAdapter
+import com.example.yumarketforowners.adapter.listener.reviewmanage.ChatRoomViewHolderListener
+import com.example.yumarketforowners.data.model.reviewmanage.ChatRoomModel
+import com.example.yumarketforowners.screen.reviewmanage.ReviewManageContract
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ChatRoomInnerFragment : BaseReviewInnerFragment<ChatRoomModel>() {
+    @Inject
+    lateinit var presenter: ReviewManageContract.Presenter<ChatRoomModel>
+
+    override val adapter: ModelRecyclerAdapter<ChatRoomModel> by lazy {
+        ModelRecyclerAdapter(
+            emptyList(),
+            object : ChatRoomViewHolderListener {
+                override fun onClicked(chatRoom: ChatRoomModel) {
+                    // TODO: 2022.07.10 start chat room activity
+                    Toast.makeText(context, "$chatRoom clicked", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onRemoveClicked(chatRoom: ChatRoomModel) {
+                    // TODO: 2022.07.10 handle remove
+                    Toast.makeText(context, "$chatRoom remove clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+
+    override fun requestData() {
+        // TODO: 2022.07.10 request by market id
+        presenter.requestData(0)
+    }
+}
